d04faa54aa4d2d477219c6f99c3c1b3ca9f1014f
turmila258-commits <turmila258@gmail.com>
2026-02-01 19:12:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const noBtn = document.getElementById("no");
+const yesBtn = document.getElementById("yes");
+const message = document.getElementById("message");
 
 noBtn.addEventListener("mouseover", () => {
   const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
@@ -7,4 +9,8 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.position = "absolute";
   noBtn.style.left = x + "px";
   noBtn.style.top = y + "px";
+});
+
+yesBtn.addEventListener("click", () => {
+  message.textContent = "YAY!! 💖 Happy Valentine’s Day 💕";
 });
